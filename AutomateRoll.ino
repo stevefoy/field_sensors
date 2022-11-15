6dc7adeb041d3c694f82f50cc3fe2bb90c725d27
steve foy <stevfoy@gmail.com>
2022-11-15 15:19:30 +0000

    Add files via upload
    week 2

--- AutomateRoll.ino
+++ AutomateRoll.ino
@@ -1,18 +1,3 @@
- /*!
-  * file AutomateRoll.ino
-  * brief Automate Roll.
-  * @n [Get the module here](等上架后添加商品购买链接)
-  * @n This example Set the volume size and play music snippet.
-  * @n [Connection and Diagram](等上架后添加wiki链接)
-  *
-  * Copyright	[DFRobot](http://www.dfrobot.com), 2016
-  * Copyright	GNU Lesser General Public License
-  *
-  * @author [yangyang](971326313@qq.com)
-  * version  V1.0
-  * date  2017-2-10
-  */
-
 #include <Wire.h>
 #include "DFRobot_RGBLCD.h"
 
